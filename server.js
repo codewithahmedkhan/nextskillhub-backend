62c5ce6741fcfd37e1f563a0b9db285c86121c77
codewithahmedkhan <codewithahmedkhan@gmail.com>
2025-04-07 21:06:40 +0400

    Add logging middleware to server.js for request tracking

--- server.js
+++ server.js
@@ -26,3 +26,10 @@ app.use((req, res, next) => {
     next();
   });
   
+
+  // Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+  });
+  
